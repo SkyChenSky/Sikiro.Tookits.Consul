a1c6f9e1698de9f7f4bb5d25649ef2084a85c040
陈珙 <skychensky@outlook.com>
2018-07-25 15:03:54 +0800

    添加项目文件。

---
+++ Sikiro.Tookits.Consul/ConfigurationExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sikiro.Tookits.Consul
+{
+    public static class ConfigurationExtensions
+    {
+        private static ConsulConfiguration _consulConfiguration;
+
+        public static IApplicationBuilder UseConsul(this IApplicationBuilder app, IApplicationLifetime lifetime, Action<ServerConfiguration> optionAction)
+        {
+            var consulClient = app.ApplicationServices.GetService<ConsulClient>();
+            if (consulClient == null)
+                throw new Exception("please AddConsul first");
+
+            var serverConfiguration = new ServerConfiguration();
+            optionAction(serverConfiguration);
+
+            var serviceRegistration = GetServiceRegistration(serverConfiguration);
+
+            //添加注册
+            consulClient.Agent.ServiceRegister(serviceRegistration).Wait();
+
+            //取消注册
+            lifetime.ApplicationStopping.Register(() =>
+            {
+                consulClient.Agent.ServiceDeregister(serviceRegistration.ID).Wait();
+            });
+            return app;
+        }
+
+        public static IServiceCollection AddConsul(this IServiceCollection serviceCollection, Action<ConsulConfiguration> optionAction)
+        {
+            _consulConfiguration = new ConsulConfiguration();
+            optionAction(_consulConfiguration);
+
+            var consulClient = new ConsulClient(x =>
+                x.Address = new Uri(_consulConfiguration.Host));
+
+            serviceCollection.AddSingleton(consulClient);
+
+            return serviceCollection;
+        }
+
+        private static Uri GetSelfUri(string uristring)
+        {
+            return new Uri(uristring);
+        }
+
+        private static AgentServiceRegistration GetServiceRegistration(ServerConfiguration serverConfiguration)
+        {
+            var localIp = GetSelfUri(serverConfiguration.SelfHost);
+
+            var serviceRegistration = new AgentServiceRegistration
+            {
+                Check = new AgentServiceCheck
+                {
+                    DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(60),
+                    Interval = TimeSpan.FromSeconds(30),
+                    HTTP = $"http://{localIp.Host}:{localIp.Port}/api/health",
+                    Timeout = TimeSpan.FromSeconds(3)
+                },
+                ID = Guid.NewGuid().ToString("N"),
+                Name = serverConfiguration.ServerName,
+                Address = localIp.Host,
+                Port = localIp.Port,
+                Tags =
+                    new[]
+                    {
+                        serverConfiguration.ServerName
+                    }
+            };
+
+            return serviceRegistration;
+        }
+    }
+
+    public class ConsulConfiguration
+    {
+        internal string Host { get; set; }
+
+        public void WithHost(string host)
+        {
+            Host = host;
+        }
+    }
+
+    public class ServerConfiguration
+    {
+        internal string ServerName { get; set; }
+
+        internal string SelfHost { get; set; }
+
+        public void WithServerName(string serverName)
+        {
+            ServerName = serverName;
+        }
+
+        public void WithSelfHost(string selfHost)
+        {
+            SelfHost = selfHost;
+        }
+    }
+
+    [Route("api/[Controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public OkResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Sikiro.Tookits.Consul/KyExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Consul;
+
+namespace Sikiro.Tookits.Consul
+{
+    public static class ConsulKyExtensions
+    {
+        public static async Task<bool> KvPutAsync(this ConsulClient consulClient, string key, string value)
+        {
+            var kvPair = new KVPair(key)
+            {
+                Value = Encoding.UTF8.GetBytes(value)
+            };
+            var result = await consulClient.KV.Put(kvPair);
+
+            if (result.StatusCode == HttpStatusCode.OK)
+                return result.Response;
+
+            return false;
+        }
+
+        public static bool KvPut(this ConsulClient consulClient, string key, string value)
+        {
+            var kvPair = new KVPair(key)
+            {
+                Value = Encoding.UTF8.GetBytes(value)
+            };
+            var result = consulClient.KV.Put(kvPair).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            if (result.StatusCode == HttpStatusCode.OK)
+                return result.Response;
+
+            return false;
+        }
+
+        public static async Task<string> KvGetAsync(this ConsulClient consulClient, string key)
+        {
+            var result = await consulClient.KV.Get(key);
+
+            return Encoding.UTF8.GetString(result.Response.Value);
+        }
+
+        public static string KvGet(this ConsulClient consulClient, string key)
+        {
+            var result = consulClient.KV.Get(key).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            return Encoding.UTF8.GetString(result.Response.Value);
+        }
+
+        public static async Task<bool> KvDeleteAsync(this ConsulClient consulClient, string key)
+        {
+            var result = await consulClient.KV.Delete(key);
+
+            if (result.StatusCode == HttpStatusCode.OK)
+                return result.Response;
+
+            return false;
+        }
+
+        public static bool KvDelete(this ConsulClient consulClient, string key)
+        {
+            var result = consulClient.KV.Delete(key).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            if (result.StatusCode == HttpStatusCode.OK)
+                return result.Response;
+
+            return false;
+        }
+    }
+}
